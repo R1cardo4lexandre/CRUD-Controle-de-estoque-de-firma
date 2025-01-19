0ca9a28f6b1861a1d8271dbcd8b7eb215e0031e0
BrunoCHT <140987363+BrunoCHT@users.noreply.github.com>
2025-01-19 15:06:31 -0300

    Update DatabaseAPI_test.cpp
    alterado para interagir com uma lista de maps

--- src/utils/DatabaseAPI_test.cpp
+++ src/utils/DatabaseAPI_test.cpp
@@ -7,15 +7,21 @@
 class DatabaseAPI_test : public DatabaseAPI {
 public:
     //Aqui é feita a leitura do item e sua quantidade.
-    std::map<std::string, std::string> read(const std::string& tableName) const override {
-        return { {"column1", "value1"}, {"column2", "value2"} };
+    std::vector<std::map<std::string, std::string>> read(const std::string& tableName) const override {
+        return {
+          { {"column1", "value1"}, {"column2", "value2"} },
+          { {"column1", "value3"}, {"column2", "value4"} }
+        };
     }
     //Aqui é feita a escrita dos dados, sobrepondo os dados que estariam nos elementos
-    void write(const std::string& tableName, const std::map<std::string, std::string>& data) override {
+    void write(const std::string& tableName, const std::vector<std::map<std::string, std::string>>& data) override {
+        //Aqui é para colocar a linha que será alterada
+        for (const auto& row : data) {
         //Este for é para fazer a "quebra" dos valores no data nas duas variáveis que ele armazena.
-        for (const auto& [column, value] : data) {
-            //fiquei na dúvida sobre esse cout, se preciso colocar ou não
-            std::cout << "" << column << ": " << value << std::endl;
+            for (const auto& [column, value] : row) {
+                //fiquei na dúvida sobre esse cout, se preciso colocar ou não
+                std::cout << "" << column << ": " << value << std::endl;
+            }
         }
     }
 };
